dd16946bca7c8a2e79e74a0fd2f942b1bbccfd7d
leejoungwoo <a2036555@gmail.com>
2026-09-06 03:22:53 +0900

    Add Quotio 0.31.0 cask

---
+++ Casks/quotio.rb
@@ -0,0 +1,17 @@
+cask "quotio" do
+  version "0.31.0"
+  sha256 "d555116de01999e9a826353e47e52af98a50c55b01d51528212f5b40b606d701"
+
+  url "https://github.com/nguyenphutrong/quotio/releases/download/v#{version}/Quotio-#{version}.dmg"
+  name "Quotio"
+  desc "Command center for AI coding assistants on macOS"
+  homepage "https://github.com/nguyenphutrong/quotio"
+
+  app "Quotio.app"
+
+  zap trash: [
+    "~/Library/Application Support/Quotio",
+    "~/Library/Caches/Quotio",
+    "~/Library/Preferences/com.nguyenphutrong.quotio.plist",
+  ]
+end
